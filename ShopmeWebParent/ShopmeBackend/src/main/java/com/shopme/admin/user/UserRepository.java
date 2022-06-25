b5dab95558b2334b6b8c68543941bbbf950b80f3
kalkithesaviour <vishal.adhikari36@gmail.com>
2022-06-25 16:46:20 +0530

    1. Icons added using fontawesome 2. Controller method editUser() - isEmailUnique() and save() method of UserService updated according to ID of the user

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -3,9 +3,11 @@ package com.shopme.admin.user;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.shopme.common.entity.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
 	@Query("SELECT u from User u WHERE u.email = :email")
